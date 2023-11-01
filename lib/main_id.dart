db871a520e5980d72a74910de3010b9cc4b722e4
유재현 <yjhleo06@gmail.com>
2023-11-01 17:23:52 +0900

    학생증 버튼

--- lib/main_id.dart
+++ lib/main_id.dart
@@ -3,7 +3,78 @@ import 'package:flutter/material.dart';
 class Main_id extends StatelessWidget {
   const Main_id ({super.key});
   @override
-  Widget build(BuildContext context){
-    return MaterialApp();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('학생증'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    radius: 50.0,
+                    backgroundImage: AssetImage('assets/profile_picture.jpg'),
+                  ),
+                  SizedBox(height: 10.0),
+                  Text(
+                    '이름: 장재준',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 5.0),
+                  Text(
+                    '학교: ABC 중학교',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  SizedBox(height: 5.0),
+                  Text(
+                    '학번: 20230001',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              '기타 정보',
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Text('성별: 남성'),
+            Text('생년월일: 2005년 1월 1일'),
+            Text('전화번호: 010-1234-5678'),
+            // 추가적인 정보를 필요에 따라 나열할 수 있습니다.
+          ],
+        ),
+      ),
+    );
   }
 }
+
+
+
+
